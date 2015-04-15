4f03978b8662b8626d8ece6e60ec7416a1e8717e
Adam Day <aday@twinfallspubliclibrary.org>
2015-04-15 10:45:06 -0600

    Updated the IR Sketch to include basic servo movements.

--- Sketches/IR Sensor/IR_Sensor/IR_Sensor.ino
+++ Sketches/IR Sensor/IR_Sensor/IR_Sensor.ino
@@ -1,27 +1,70 @@
+//Include the servo library
+#include <Servo.h>
+
+//Assign servo objects
+Servo rightServo;
+Servo leftServo;
+
+//Assign variables for servo directions
+/*
+Max is 180 and rotates the servo in one direction.
+Min is 0 and rotates the servo in the opposite direction. 
+Middle is 90 and stops the servo from moving.
+*/
+const int rightForward = 0;
+const int rightBackward = 180;
+
+const int leftForward = 180;
+const int leftBackward = 0; 
+
+const int brake = 90;
+
+//Assign variable for LED
+const int led = 3;
+
+//Assign variable for IR sensor
 const int irSensor = A0; 
 
-//Lower = farther away
-//Higher = closer
+//Assign variables for range detections.
+/*
+The lower the number the farther away the object is.
+The higher the number the closer the object is. 
+*/
 const int target = 250;
 
 void setup()
 {
+  //Start Serial connection for troubleshooting
   Serial.begin(9600);
+  
+  //Assign pins to servo objects
+  rightServo.attach(6);
+  leftServo.attach(5);
+  
+  //Sets LED pin as an output for an indicator light.
+  pinMode(led, OUTPUT);
 }
 
 void loop()
 {
+  //Avoidance logic
+  /*
+  Check the returned value of range() and compare it to the target variable.
+  */
   if(range() >= target)
   {
-     Serial.println("Turn"); 
+     digitalWrite(led, HIGH);
+     right();
   }
   else
   {
-    Serial.println("Forward");
+    digitalWrite(led, LOW);
+    forward();
   }
   delay(100);
 }
 
+//Uses the IR sensor and returns a value representing the range of the object. 
 int range()
 {
   int distance[3];
@@ -43,4 +86,32 @@ int range()
   average = average / 3;
   
   return average;
+}
+
+void forward()
+{
+  rightServo.write(rightForward);
+  leftServo.write(leftForward);
+  Serial.println("Forward");
+}
+
+void left()
+{
+  rightServo.write(rightForward);
+  leftServo.write(leftBackward);
+  Serial.println("Left");
+}
+
+void right()
+{
+  rightServo.write(rightBackward);
+  leftServo.write(leftForward);
+  Serial.println("Right");
+}
+
+void allStop()
+{
+   rightServo.write(brake); 
+   leftServo.write(brake);
+   Serial.println("Stop");
 }
